03e3c49123b8fe620e87f3c6e20e105817215b49
Damien <wu20880@bristol.ac.uk>
2022-10-19 17:37:07 +0100

    sync

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 #!/user/bin/python
 
 import gpiozero as io
-import sense_hat as sense
+from sense_hat import SenseHat
 from time import sleep
 
-led1 = io.LED(17)
-led2 = io.LED(27)
-
+sense = SenseHat()
 while True: 
-    led1.on()
-    sleep(0.5)
-    led2.on()
-    sleep(0.5)
-    led1.off()
-    sleep(0.5)
-    led2.off()
-    sleep(0.5)
-    print("Done cycle")
+    X = [255,255,255]
+    O = [0,0,0]
+    question_mark = [
+O, O, O, X, X, O, O, O,
+O, O, X, O, O, X, O, O,
+O, O, O, O, O, X, O, O,
+O, O, O, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, X, O, O, O, O
+]
+    sense.set_pixels(question_mark)
 
