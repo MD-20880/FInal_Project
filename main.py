0e59d0fae8c67aed793645353c4cde9a39ce80be
Damien <wu20880@bristol.ac.uk>
2022-10-19 16:56:37 +0100

    sync

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ led2 = io.LED(27)
 
 while True: 
     led1.on()
-    sleep(1)
+    sleep(0.5)
     led2.on()
-    sleep(1)
+    sleep(0.5)
     led1.off()
-    sleep(1)
+    sleep(0.5)
     led2.off()
-    sleep(1)
+    sleep(0.5)
+    print("Done cycle")
 
