f225e6bca3ed7b1f2f35d088fecbef23814be915
Damien <wu20880@bristol.ac.uk>
2022-10-19 16:44:28 +0100

    sync

--- main.py
+++ main.py
@@ -2,18 +2,18 @@
 
 import gpiozero as io
 import sense_hat as sense
-
+from time import sleep
 
 led1 = io.LED(17)
 led2 = io.LED(2)
 
 while True: 
     led1.on()
-    io.sleep(1)
+    sleep(1)
     led2.on()
-    io.sleep(1)
+    sleep(1)
     led1.off()
-    io.sleep(1)
+    sleep(1)
     led2.off()
-    io.sleep(1)
+    sleep(1)
 
